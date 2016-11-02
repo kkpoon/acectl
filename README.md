5b4f3da318705cf9ef035a364e27d5c3d519dc6f
kkpoon <noopkk@gmail.com>
2016-11-02 16:24:22 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# acectl
-ACE Project cli
+# acectl - ACE Project command line control
+
+## Install
+
+### Build It Yourself
+
+You have to install golang v1.7 or above
+
+```shell
+$ go install github.com/kkpoon/acectl
+$ acectl help
+```
+
+## usage
+
+### Login
+
+The login command helps you to get the authentication token from ACEProject
+and store it in your home directory, default: `$HOME/.acectl.json`
+
+```shell
+$ acectl login
+```
+
+### More usage
+
+```shell
+$ acectl help
+```
--- cmd/project.go
+++ cmd/project.go
@@ -15,15 +15,4 @@ var projectCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(projectCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// projectCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// projectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
